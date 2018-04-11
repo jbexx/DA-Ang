937922d5e762dd7bea509e7e55a397d3f6b0c18e
Jonathan Beckman <jbecks.media@gmail.com>
2018-04-11 16:38:25 -0600

    add member-list component and attempt to add ngxgallery, but is not successfull due to ngx-bootstrap sucking, and bootstrap sucking in general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
-import { MemeberListComponent } from './memeber-list/memeber-list.component';
+import { MemeberListComponent } from './members/memeber-list/memeber-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { AuthModule } from './auth/auth.module';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 @NgModule({
   declarations: [
@@ -24,18 +29,23 @@ import { AuthGuard } from './_guards/auth.guard';
     RegisterComponent,
     MemeberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AuthModule,
+    NgxGalleryModule
   ],
   providers: [
     AuthService,
     AlertifyService,
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,19 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemeberListComponent } from './memeber-list/memeber-list.component';
+import { MemeberListComponent } from './members/memeber-list/memeber-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: '',
     // suppose to have below but getting errors.  suppose to import from @angular/router but get errors there aswell
+    // see https://angular.io/api/router/RunGuardsAndResolvers
     //RunGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemeberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent },
     ]
